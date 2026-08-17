9fa8784e212d9957f6c7ca2f160933396dc278af
IT112-GallanaCharlynMae <gallana.charlynmae.t@gmail.com>
2026-08-17 21:56:56 +0800

    Add files via upload

---
+++ LE1-Gallana_Charlyn Mae.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("=================================================\n");
+    printf("|                                               |\n");
+    printf("|        ** COMPUTER PROGRAMMING 1 **           |\n");
+    printf("|                                               |\n");
+    printf("|             W E L C O M E !                   |\n");
+    printf("|                                               |\n");
+    printf("|        +--------------------------+           |\n");
+    printf("|        |   THINK  ->  CODE  ->    |           |\n");
+    printf("|        |   CREATE  ->  DEBUG!     |           |\n");
+    printf("|        +--------------------------+           |\n");
+    printf("|                                               |\n");
+    printf("|       WHERE IDEAS BECOME PROGRAMS             |\n");
+    printf("|                                               |\n");
+    printf("|              LET'S START!                     |\n");
+    printf("|                                               |\n");
+    printf("=================================================\n");
+
+    return 0;
+}
